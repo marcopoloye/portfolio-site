6c4f88fe4486571e28a63e180a8f487d1f0f8f69
Marco Ye <marco.ye99@gmail.com>
2022-09-12 19:30:22 -0400

    added header items

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <>
+            <ul>
+                <li>
+                    Projects
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Header;
