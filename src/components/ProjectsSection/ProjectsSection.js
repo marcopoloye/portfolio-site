1708e05ae8b6bc65410d37416ca7c806d198aee7
Marco Ye <marco.ye99@gmail.com>
2022-09-19 11:26:16 -0400

    edited layout of mobile thumbnails

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -4,14 +4,17 @@ function ProjectsSection() {
     return (
         <>
             <section className="projects-section">
-                <div className='projects-section__thumbnail' id='thumbnail-one'>
-                    
+                <div className='projects-section__thumbnail'>
+                    <div id='thumbnail-one'></div>
                 </div>
-                <div className='projects-section__thumbnail' id='thumbnail-two'>
-                    
+                <div className='projects-section__thumbnail'>
+                    <div id='thumbnail-two'></div>
                 </div>
                 <div className='projects-section__thumbnail' id='thumbnail-three'>
-                    
+                    <div id='thumbnail-three'></div>
+                </div>
+                <div className='projects-section__thumbnail' id='thumbnail-three'>
+                    <div id='thumbnail-four'></div>
                 </div>
             </section>
         </>
