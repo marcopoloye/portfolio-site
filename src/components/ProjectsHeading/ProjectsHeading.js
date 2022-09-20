d7112753be2707b195b668e9ef43bf1a9885137a
Marco Ye <marco.ye99@gmail.com>
2022-09-20 03:31:32 -0400

    added custom font and changed thumbnails

--- src/components/ProjectsHeading/ProjectsHeading.js
+++ src/components/ProjectsHeading/ProjectsHeading.js
@@ -3,7 +3,7 @@ import './ProjectsHeading.scss'
 function ProjectsHeading() {
     return (
         <>
-            <h2>Projects</h2>
+            <h2 className='projects_heading'>Projects</h2>
         </>
     );
 }
