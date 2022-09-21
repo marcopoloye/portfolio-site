273b73036f9f2d77f5316e0c7461f62542da0060
Marco Ye <marco.ye99@gmail.com>
2022-09-20 20:35:31 -0400

    added intro section and adjusted margins

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
 import ContactsSection from "./components/ContactsSection/ContactsSection";
+import IntroSection from "./components/IntroSection/IntroSection";
 
 function App() {
   return (
     <>
       {/* <Header/> */}
+      <IntroSection/>
       <ProjectsSection/>
       <ContactsSection/>
     </>
