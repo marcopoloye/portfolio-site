94b61412d3be12cc0f21d5affcddc948e709b2fc
Marco Ye <marco.ye99@gmail.com>
2022-09-21 03:11:49 -0400

    added basic form

--- src/components/ContactsSection/ContactsSection.js
+++ src/components/ContactsSection/ContactsSection.js
@@ -3,9 +3,15 @@ import './ContactsSection.scss'
 function ContactsSection() {
     return (
         <>
-            <h2 className="contacts-section__heading">Contact</h2>
+            <h2 className='contacts-section__heading'>Contact</h2>
 
-            <div></div>
+            <div>
+                <form className='contacts-section__form'>
+                    <input placeholder='Enter your name' type='text'></input>
+                    <input placeholder='Enter your email' type='text'></input>
+                    <textarea placeholder='Enter your message'></textarea>
+                </form>
+            </div>
         </>
     );
 }
