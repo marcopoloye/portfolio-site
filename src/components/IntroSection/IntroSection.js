3f256b72cac96cea55f0abf0f7f4c0a41c9f30e1
Marco Ye <marco.ye99@gmail.com>
2022-09-20 20:48:00 -0400

    adjusted margins

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -9,7 +9,7 @@ function IntroSection () {
             <section className='intro-section__body'>
                 <p className='intro-section__text'>Hi my name is Marco! I'm a full stack web developer with an interest in front-end development.</p>
                 <p className='intro-section__text'>I have a background in biology, chemistry, and data visualisation, but in August 2022 I graduated from BrainStation's Web Dev Bootcamp and have been coding ever since!</p>
-                <p className='intro-section__text'>Below is some of my work and contact info, let's connect!</p>
+                <p className='intro-section__text bottom-border'>Below is some of my work and contact info, let's connect!</p>
             </section>
             
 
