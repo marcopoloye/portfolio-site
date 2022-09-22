bfb320673310919e1bf96646b774451407b92668
Marco Ye <marco.ye99@gmail.com>
2022-09-22 02:48:10 -0400

    fixed wrapping on header and photo at larger viewpoints

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -3,8 +3,11 @@ import './IntroSection.scss'
 function IntroSection () {
     return (
         <div className='intro-section'>
-            <h2 className='intro-section__heading'>Marco Ye</h2>
-            <div className='intro-section__photo-border'></div>
+            <div className='intro-section__heading-photo-container'>
+                <h2 className='intro-section__heading'>Marco Ye</h2>
+                <div className='intro-section__photo'></div>
+            </div>
+
             <section className='intro-section__body'>
                 <p className='intro-section__text'>Hi I'm Marco! I'm a full stack web developer with an interest in front-end development.</p>
                 <p className='intro-section__text'>I have a background in biology, chemistry, and data visualisation, but in August 2022 I graduated from BrainStation's Web Dev Bootcamp and have been coding ever since!</p>
