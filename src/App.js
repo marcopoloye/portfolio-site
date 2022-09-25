6ae442e5cf1a57cd98334c7171dcd7c7bd92f1d0
Marco Ye <marco.ye99@gmail.com>
2022-09-24 21:07:16 -0400

    edited css code and removed header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Header from "./components/Header/Header";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
 import ContactSection from "./components/ContactSection/ContactSection";
 import IntroSection from "./components/IntroSection/IntroSection";
@@ -7,7 +6,6 @@ import SkillsSection from "./components/SkillsSection/SkillsSection";
 function App() {
   return (
     <div className='app'>
-      {/* <Header/> */}
       <IntroSection/>
       <ProjectsSection/>
       <SkillsSection/>
